4988f2101f91a68b8aef0cd2579acaf6ace0f124
unknown <robertomota.contato@gmail.com>
2022-11-08 20:28:25 -0300

    carregado somente dados

--- main_script.py
+++ main_script.py
@@ -3,30 +3,59 @@ from openpyxl import load_workbook
 path = "C:/Users/Roberto/Desktop/exemplo.xlsx"
 
 tipo = 4
-codigo = 2
+item = 2
+quantidade = 6
+descricao = 3
 
 
 def format(caminhoOriginal, filtro):
     planilha = load_workbook(caminhoOriginal)
     sheets = planilha.sheetnames[0]
     folha = planilha[sheets]
+
     lista = selectRows(folha, filtro)
-    print(lista)
+    dados = selectData(folha, lista)
 
     planilha.close()
 
 
-def selectRows(folha, filtro):
+def selectRows(folha, filtro) -> list:
     contador = 0
     linhas = list()
     for rows in folha:
-        # print(f'{rows[2].value}  {rows[4].value}')
         contador += 1
-        if filtro in rows[codigo].value and 'P' in rows[tipo].value:
-            print(f'{contador} - {rows[codigo].value}  {rows[tipo].value}')
+        if filtro in rows[item].value and 'P' in rows[tipo].value:
+            # print(f'{rows[item].value}  {rows[tipo].value}  {rows[descricao].value}   {rows[quantidade].value}')
             linhas.append(contador)
-
     return linhas
+
+
+def selectData(folha, lista) -> list:
+    _item = 'C'
+    _descricao = 'D'
+    _tipo = 'E'
+    _quantidade = 'G'
+    contador = 0
+    Colunas = []
+    Matriz = []
+    for row in folha:
+        contador += 1
+        indexMatriz = 0
+        for index in lista:
+            if contador == index:
+                # print(f'{folha.cell(row=index, column=item + 1).value}  {row[tipo].value}')
+
+                Colunas.append(folha.cell(row=index, column=item + 1).value)
+                Colunas.append(folha.cell(row=index, column=descricao + 1).value)
+                Colunas.append(folha.cell(row=index, column=quantidade + 1).value)
+                Colunas.append(folha.cell(row=index, column=tipo + 1).value)
+                Matriz.append(Colunas.copy())
+                Colunas.clear()
+
+    for i in Matriz:
+        print(i)
+
+    return Colunas
 
 
 if __name__ == "__main__":
